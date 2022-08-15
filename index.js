d141d34f65cf7df0e111a3c8f0f690104f2452b4
David <david.njoroge@student.moringaschool.com>
2022-08-15 22:34:08 +0300

    event listeners

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+  const buttonClick=document.getElementById("button");
+
+  function clickAlert(){
+      alert("Button has been clicked!");
+
+  }
+  buttonClick.addEventListener("click", clickAlert);
 }
